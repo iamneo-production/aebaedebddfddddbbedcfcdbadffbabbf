60e288851750d599ce34c2cb94b2ea37d6398afb
Iamneo <techadmin@iamneo.ai>
2023-08-09 08:47:45 +0000

    Backup Commit

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,22 +1,3 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -29,9 +10,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
