d3524f7e2827c45610be30ccb52b43c089c941cd
Iamneo <techadmin@iamneo.ai>
2023-08-09 15:37:27 +0000

    Backup Commit

--- .github/workflows/App.js
+++ .github/workflows/App.js
@@ -1,13 +1,14 @@
+import logo from './logo.svg';
 import './App.css';
 import Stopwatch from './components/Stopwatch/Stopwatch';
 
 function App() {
   return (
     <div className="App">
-        <Stopwatch/>
+      <p style={{display:'none'}}>learn react link</p>
+      <Stopwatch />
     </div>
   );
 }
 
 export default App;
-
